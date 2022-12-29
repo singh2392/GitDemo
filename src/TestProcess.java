360c263229eaca45da1502c4efe21804fe953236
trilok <triloksingh2392@gmail.com>
2022-12-29 12:14:11 +0530

    Code commited by Architech GitX

--- src/TestProcess.java
+++ src/TestProcess.java
@@ -4,4 +4,5 @@ public class TestProcess
 		//test comment1
 		//test comment by GitX employee
 		//Architech make some changes in the framwork
+		//GitX Architech make some changes in the framwork
 }
